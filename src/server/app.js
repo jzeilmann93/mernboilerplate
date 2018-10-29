5ab836dd32933ceb4963f51aea881e5028adb843
Josh Zeilmann <jzeilmann93@gmail.com>
2018-10-28 22:40:51 -0500

    Init commit

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import path from 'path';
+
 
 const app = express();
+const publicPath = path.resolve(__dirname, '..', '..', 'public');
+
+app.use(express.static(publicPath));
 
 app.get('/', (req,res) => {
     res.send('Hello world');
 });
 
 app.listen(3000, () => {
-    console.log('Mern Boilerplate is listening on port 3000');
+    console.log(`Mern Boilerplate is listening on port 3000 and looking in folder ${publicPath}`);
 })
